c9850c491c0d320aa952f9b8b6d68e4e2d3b1663
Koit Kõrvel <kkorvel@itcollege.ee>
2017-11-28 15:55:44 +0200

    Tehtud!

--- src/Teliapraktiline.java
+++ src/Teliapraktiline.java
@@ -3,6 +3,29 @@
  */
 public class Teliapraktiline {
     public static void main(String[] args) {
-        System.out.println("Tere!");
+
+        angle(24, 00);
+    }
+
+    public static double angle(int h, int m) {
+        double hAngle = 0.5D * (h * 60 + m);
+        double mAngle = 6 * m;
+        double angle = Math.abs(hAngle - mAngle);
+        if (h < 0 || m < 0 || h > 24 || m > 60) {
+
+            System.out.println("Wrong input!");
+
+        } else {
+
+            angle = Math.min(angle, 360 - angle);
+            System.out.println(angle);
+            return angle;
+        }
+        return angle;
     }
 }
+
+
+
+
+
